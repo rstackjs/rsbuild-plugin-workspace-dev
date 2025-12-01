8561c9a3758b4c6195da9dd367d9745147fe08f0
nyqykk <nyqykk@foxmail.com>
2025-12-01 14:16:44 +0800

    chore: enable advancedEsm

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
   lib: [
-    { format: 'esm', syntax: 'es2021', dts: true },
+    {
+      format: 'esm',
+      syntax: 'es2021',
+      dts: true,
+      experiments: { advancedEsm: true },
+    },
     { format: 'cjs', syntax: 'es2021' },
   ],
 });
