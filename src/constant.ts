14bfef5bf3e73960fae75a2b264874cebc0fa562
nyqykk <65393845+nyqykk@users.noreply.github.com>
2025-12-22 18:59:30 +0800

    fix: skip project which has not define start command && remove useless match (#11)

--- src/constant.ts
+++ src/constant.ts
@@ -2,5 +2,4 @@ export const PACKAGE_JSON = 'package.json';
 export const PLUGIN_LOG_TITLE = '[Rsbuild Workspace Dev Plugin]: ';
 
 export const RSLIB_READY_MESSAGE = 'build complete, watching for changes';
-export const MODERN_MODULE_READY_MESSAGE = 'Watching for file changes';
 export const TSUP_READY_MESSAGE = 'Watching for changes in';
