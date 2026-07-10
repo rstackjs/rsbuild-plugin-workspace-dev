7d31ee7f305514fc60b201d05be6eee04e4257cf
neverland <chenjiahan.jait@bytedance.com>
2026-07-10 17:38:57 +0800

    chore: simplify Rstest setup

--- test/rstest.config.ts
+++ test/rstest.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
-  projects: [
-    {
-      name: 'unit',
-      exclude: ['./**/*.pw.test.ts'],
-    },
-    {
-      name: 'e2e',
-      env: {
-        // Let Rsbuild choose the mode based on the command.
-        NODE_ENV: undefined,
-      },
-      include: ['./**/*.pw.test.ts'],
-      retry: process.env.CI ? 3 : 0,
-      testTimeout: 10_000,
-    },
-  ],
+  env: {
+    // Let Rsbuild choose the mode based on the command.
+    NODE_ENV: undefined,
+  },
+  retry: process.env.CI ? 3 : 0,
+  testTimeout: 10_000,
 });
